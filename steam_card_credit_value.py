9e4ce82c206059f9ceceba443c3074f2fa2f5c74
Sangdenuit <kenny.westman87@gmail.com>
2020-08-18 00:25:22 +0200

    added glorified scratch marks and removed need for pre-calculations

--- steam_card_credit_value.py
+++ steam_card_credit_value.py
@@ -1,12 +1,14 @@
 while True:
+    total_credit = float(input("Enter total credit value: "))
+    total_float = float(input("Enter total float value: "))
+    total_steam = float(input("Enter total steam value: "))
+
+    y = total_credit / (100 * total_float)
+    z = total_credit / (100 * total_steam)
     # 3 is the minimum card float value I have seen to date.
     x = 3
     a = 0
     b = 0   
-    # y = Total card credits divided by total card float value.
-    y = float(input())
-    # z = Total card credits divided by total card lifetime average value.
-    z = float(input())
     # The list will increase until the max credit value of 25 is reached.
     while (a <= 25) and (b <= 25):
         a, b = round((x * y), 2), round((x * z), 2)
